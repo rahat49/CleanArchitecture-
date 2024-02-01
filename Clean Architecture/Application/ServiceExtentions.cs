2eec1b9fc892cb55b892da451082fa8e7c28f948
Rahat <rahatsarker40@gmailcom>
2024-02-01 23:39:49 +0600

    First

---
+++ Clean Architecture/Application/ServiceExtentions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application
+{
+    public static class ServiceExtentions
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+           services.AddMediatR(conf=>conf.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
+        }
+    }
+}
---
+++ Clean Architecture/Persistence/ServiceExtentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public static class ServiceExtentions
+    {
+        public static void AddPersistence(this IServiceCollection services)
+        {
+
+        }
+    }
+}
